d33c88200c6ae585bbdd04b661d71ffc4e84c9d5
dile2001 <di.abaki@gmail.com>
2024-02-25 21:10:54 +0400

    add form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,17 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../cart.service';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {}
   items = this.cartService.getItems();
-  
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
